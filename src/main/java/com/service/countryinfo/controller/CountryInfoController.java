6fb17f4642b5a31b459d5a4196c2dafcb63a9076
Vasanth <vasanth.fine@yahoo.com>
2024-04-06 14:47:20 +0300

    added Reponse handler class.

--- src/main/java/com/service/countryinfo/controller/CountryInfoController.java
+++ src/main/java/com/service/countryinfo/controller/CountryInfoController.java
@@ -1,13 +1,17 @@
 package com.service.countryinfo.controller;
 
 import com.service.countryinfo.service.CountryInfoService;
+import com.service.countryinfo.util.ResponseHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * *
+ *
+ * @author kvasanthekumar
  */
 @RestController
 public class CountryInfoController {
@@ -16,8 +20,8 @@ public class CountryInfoController {
     CountryInfoService countryInfoService;
 
     @GetMapping(value = "country/into/{name}")
-    public String getCountryInfoByName(@PathVariable String name) {
+    public ResponseEntity<Object> getCountryInfoByName(@PathVariable String name) {
         System.out.println("Application Running......");
-        return countryInfoService.getCountryInfo(name);
+        return ResponseHandler.responseBuilder("Requested country details are given here", HttpStatus.OK, countryInfoService.getCountryInfo(name));
     }
 }
